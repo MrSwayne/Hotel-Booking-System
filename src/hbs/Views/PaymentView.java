7f8bb9ce1ade7fe282ad8a43891c3cb7df958c91
Adam Swayne <adam@swayne.ie>
2018-11-07 13:29:52 +0000

    Changed path

---
+++ src/hbs/Views/PaymentView.java
@@ -0,0 +1,28 @@
+package hbs.views;
+
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+
+public class PaymentView extends JFrame implements ActionListener {
+
+	
+	public PaymentView() {
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+}
