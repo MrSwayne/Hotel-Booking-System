a7650546e5f274d5c9fbca19634b327e27a464fe
Adam Swayne <MrSwayne@users.noreply.github.com>
2018-11-07 13:28:01 +0000

    Fixed bugs v2

---
+++ src/models/HotelModel.java
@@ -0,0 +1,32 @@
+package hbs.models;
+
+public class HotelModel
+{
+	private int rooms, hID;
+	private String name, city, country;
+	
+	public int getRooms()
+	{
+		return rooms;
+	}
+	
+	public int getHID()
+	{
+		return hID;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public String getCity()
+	{
+		return city;
+	}
+	
+	public String getCountry()
+	{
+		return country;
+	}
+}
