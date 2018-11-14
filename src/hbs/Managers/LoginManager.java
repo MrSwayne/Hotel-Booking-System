e362311bab4edaf678ff501578d2f6a47c29411d
Patryk Marczak <paddy760@gmail.com>
2018-11-14 15:33:50 +0000

    Updated Login

--- src/hbs/Managers/LoginManager.java
+++ src/hbs/Managers/LoginManager.java
@@ -3,22 +3,38 @@ package hbs.Managers;
 
 import hbs.Models.LoginModel;
 import hbs.Views.LoginView;
-import hbs.database.LoginDBController;
-//import view and model.
+import hbs.Views.MainMenuView;
+
 public class LoginManager{
+    	private LoginModel model;
+    	private LoginView view; 
 	public LoginManager(LoginView view)
 	{
-		new LoginModel();
+	    model = new LoginModel();
+	    this.view = view;
 	}
 
-	public boolean checkCredentials(String username,String password){
-		LoginDBController db = new LoginDBController();
-		db.verifyLoginDetails(username, password);
-		if(db.validation(username, password))
+	public void checkCredentials(String username,String password)
+	{
+	    //change it to a method in model which will set the info from the database.
+	    model.setUsername("Patryk");
+	    model.setPassword("easy");
+	    if(username != null && password != null)
+	    {
+		if(validation(username, password))
 		{
-			return true;
+		    view.remove();
 		}
 		else {
+			System.exit(0);
+			view.remove();
+	    		}
+	    }   
+	}
+	public boolean validation(String name,String password) {
+		if(name.equals(model.getUsername()) && password.equals(model.getPassword())){
+			return true;
+		}else {
 			return false;
 		}
 	}
