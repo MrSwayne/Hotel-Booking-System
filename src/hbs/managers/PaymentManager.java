65e51bee53723116b5ca070db36367f844d636ce
Adam Swayne <MrSwayne@users.noreply.github.com>
2018-11-21 10:20:23 +0000

    Add files via upload

--- src/hbs/managers/PaymentManager.java
+++ src/hbs/managers/PaymentManager.java
@@ -1,13 +1,14 @@
 package hbs.managers;
 
+import hbs.models.BookingModel;
 import hbs.models.GuestModel;
 import hbs.models.PaymentModel;
 import hbs.views.PaymentView;
 
-/*
+
 import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
-import com.stripe.model.Charge;*/
+import com.stripe.model.Charge;
 
 public class PaymentManager {
 	
@@ -16,14 +17,36 @@ public class PaymentManager {
 	public PaymentManager() {
 	}
 	
-	public void processPayment(String firstName, String secondName, String cNumber) {		
-		//Stripe.apiKey = API_KEY;
+	public void processPayment(String name, String cNumber, String expiry, String security) {		
+		Stripe.apiKey = API_KEY;
 		
-		//GuestModel guest = new GuestModel();
-		//guest.setFirstName(firstName);
+		GuestModel guest = new GuestModel();
+		String[] names = name.split(" ");
 		
-		////guest.setSecondName(secondName);
-		//guest.getCredentials
+		if(names.length == 2) {
+			guest.setFirstName(names[0]);
+			guest.setLastName(names[1]);
+		} else if(names.length == 3) {
+			guest.setFirstName(names[0]);
+			guest.setLastName(names[2]);
+		} else {
+			guest.setFirstName(names[0]);
+		}
+		/*
+		
+		if(guest.getCredentials()) {
+			int GID = guest.getGID();
+			
+			BookingModel booking = new BookingModel();
+			booking.setGID(GID);
+			
+			if(booking.getCredentials()) {
+				
+			} else
+				System.err.println("Error retrieving bookings");
+		} else
+			System.err.println("Error retrieving guest");
+			*/
 	}
 }
 
