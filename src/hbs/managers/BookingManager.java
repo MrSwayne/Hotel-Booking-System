c2d87282abd44df31370239d4e2c93fa616c3360
Adam Swayne <adam@swayne.ie>
2018-12-03 02:31:25 +0000

    wtf did I just do

---
+++ src/hbs/managers/BookingManager.java
@@ -0,0 +1,152 @@
+package hbs.managers;
+
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import hbs.controllers.BookingController;
+import hbs.decorator.Discount;
+import hbs.decorator.Gold;
+import hbs.decorator.Platinum;
+import hbs.decorator.Silver;
+import hbs.models.BookingModel;
+import hbs.models.GuestModel;
+import hbs.models.RoomModel;
+//import view and model.
+public class BookingManager{
+    private GuestModel guestM = new GuestModel();
+    private RoomModel roomM = new RoomModel();
+    private BookingModel bookingM = new BookingModel();
+    final static String dateFormat = "dd/MM/yy";
+    
+    public BookingManager()
+	{
+	}
+    public void checkBooking(String firstName, String lastName, String dateIn, String dateOut, String roomAmount,
+	    String roomType) 
+    {
+	//hard coded at the moment -> change it later...
+	bookingM.getInformation();
+<<<<<<< HEAD
+	//guestM.getInformation(); DB
+	roomM.getInformation(); 
+	guestM.setMemLev(1);
+=======
+	//guestM.getInformation(); 
+	//roomM.getInfortmation(); 
+	roomM.setRoomNumber(50);	
+	roomM.setType("Double");	
+	roomM.setPrice(60);	
+	roomM.setAvailability(true);
+	//guestM.setMemLev(1);
+	//bookingM.setGID(0);
+>>>>>>> a93156980722d6a7b46926c160b874450b8655d9
+	//check if the name correspond to any in the db otherwise set everything as a new customer.
+	if(dateValidation(dateIn,dateOut))
+	{
+	    System.out.println("Hurray,dates are valid");
+	    if(roomsAvailable(roomAmount,roomType))
+	    {
+		bookingProgram(firstName,lastName,dateIn,dateOut,roomAmount,roomType);
+	    }
+	}
+    }
+
+	public boolean dateValidation(String dateIn,String dateOut)
+	{
+	  if(dateIn == null || dateOut == null)
+	  {
+	      return false;
+	  }
+	  
+	  SimpleDateFormat sdf = new SimpleDateFormat(dateFormat);
+	  sdf.setLenient(false);
+	  
+	  try {
+	      //if not valid, it will throw ParseException
+	      Date date = sdf.parse(dateIn);
+	      Date date2 = sdf.parse(dateOut);
+	      bookingM.setDateIn(dateIn);
+	      bookingM.setDateOut(dateOut);
+	      long diff = date2.getTime() - date.getTime();
+	      int nights = (int) (diff/ (1000*60*60*24));
+	      bookingM.setAmountOfNights(nights);
+	      System.out.println(date + " " + date2);
+	  }catch (ParseException e) {
+	      	  System.out.println("Invalid date(s)");
+	      	  return false;
+	  }
+	  return true;
+	}
+	
+	public boolean roomsAvailable(String roomAmount,String roomType)
+	{
+	    if(roomAmount != null || roomType != null){
+		if(roomAmount.matches("[0-9]+"))
+		{
+		    int rmNumber = Integer.parseInt(roomAmount);
+		    if((rmNumber <= 50||roomType.equals(roomM.getType())))
+		    {
+			if(roomM.getAvailability())
+			{
+			    System.out.println("Hurray,room is available");
+			    return true;
+			}
+		    }
+		} 
+	    }
+	    return false; 
+	}
+	
+	public double calculateTotalSpent(String firstName, String lastName, String dateIn, String dateOut, String roomAmount,String roomType) {
+		guestM.setFirstName(firstName);
+	    guestM.setLastName(lastName);
+	    Discount silver = new Silver();
+	    Discount gold = new Gold();
+	    Discount plat = new Platinum();
+	    int memLev = guestM.getMemLev();
+	    int rmBooked = Integer.parseInt(roomAmount);
+	    double totalSpent;
+	    double roomCost = roomM.getPrice();
+	    int nights = bookingM.getAmountOfNights();
+	    double discount;
+	    if(memLev == 1)
+	    {
+		silver.setDiscount(100.00);
+		discount = silver.getDiscount();
+		totalSpent = ((roomCost * nights) * rmBooked) - discount;
+		guestM.setTotalSpent(totalSpent);
+		bookingM.setBooking();
+	    }else if(memLev == 2)
+	    {
+		gold.setDiscount(200.00);
+		discount = gold.getDiscount();
+		totalSpent = ((roomCost * nights) * rmBooked) - discount;
+		guestM.setTotalSpent(totalSpent);
+		bookingM.setBooking();
+	    }
+	    else if(memLev == 3)
+	    {
+		plat.setDiscount(300.00);
+		discount = plat.getDiscount();
+		totalSpent = ((roomCost * nights) * rmBooked) - discount;
+		guestM.setTotalSpent(totalSpent);
+		bookingM.setBooking();
+	    }else
+	    {
+		totalSpent = ((roomCost * nights) * rmBooked);
+		guestM.setTotalSpent(totalSpent);
+		//bookingM.setBooking();
+	    }
+	    return totalSpent;
+	}
+	
+	public void bookingProgram(String firstName, String lastName, String dateIn, String dateOut, String roomAmount,String roomType)
+	{
+	    double totalSpent = calculateTotalSpent(firstName, lastName, dateIn, dateOut, roomAmount, roomType);
+	    System.out.println("You have booked a reservation with ID" + bookingM.getBID() + " total cost = " + totalSpent);
+	    BookingController bookingC = new BookingController();
+	    bookingC.notifyObservers();
+	}
+}
---
+++ src/hbs/models/BookingModel.java
@@ -0,0 +1,116 @@
+package hbs.models;
+
+
+
+import hbs.database.DatabaseHelper;
+import hbs.database.Query;
+
+public class BookingModel extends Model
+{
+	private int gID, rID, bID,nights; 
+	private String date_in, date_out;
+	final static String dateFormat = "dd/MM/yy";
+	
+	public BookingModel()
+	{
+		
+	}
+	
+	public int getGID()
+	{
+		return gID;
+	}
+	public void setGID(int gID)
+	{
+		this.gID=gID;
+	}
+	
+	public int getRID()
+	{
+		return rID;
+	}
+	
+	public int getBID()
+	{
+		return bID;
+	}
+	
+	public void setBID(int bID)
+	{
+	    this.bID= bID;
+	}
+
+	public void setDateIn(String date)
+	{
+		this.date_in = date;
+	}
+	
+	public String getDateIn()
+	{
+		return date_in;
+	}
+	
+	public void setDateOut(String date)
+	{
+		this.date_out = date;
+	}
+	
+	public String getDateOut()
+	{
+		return date_out;
+	}
+	
+	public int getAmountOfNights()
+	{
+	    return nights;
+	}
+	
+	public void setAmountOfNights(int nights)
+	{
+	    this.nights=nights;
+	}
+
+	public void getInformation() {
+	    DatabaseHelper db = DatabaseHelper.getInstance();
+	    Query query = db.executeQuery("select * from bookings;");
+	    Query query2 = db.executeQuery("Select * from guests");
+	  	for(int i=0;i<query.size();i++)
+	  	{
+	  	  int bID = Integer.parseInt(query.get(i).get("Bid"));
+	  	  
+		    String date_In = (query.get(i).get("dateIn"));
+		    String date_Out = (query.get(i).get("dateOut"));	  
+		   this.setGID(Integer.parseInt(query2.get(i).get("Gid")));
+	  	  
+	  	  if(this.getBID()!=bID)
+	  	  {
+	  	      if(this.getDateIn().equals(date_In) && this.getDateOut().equals(date_Out))
+	  	      {
+	  		  System.out.println("Those dates are already taken");
+	  	      }
+	  	      else {
+	  		  //do something
+	  	      }
+	  	  }
+	  	  else
+	  	  {
+	  	      System.out.println("This bookign alreayd exists");
+	  	  }
+	  	  
+	  	}
+	    
+	}
+	//MEthod to insert new booking into the db
+	public void setBooking() {
+	    DatabaseHelper db = DatabaseHelper.getInstance();
+	     db.executeQuery("INSERT INTO bookings VALUES(default,"+this.getDateIn()+","+this.getDateOut()+","+this.getGID()+"," + this.getRID()+");");	    
+	}
+
+	@Override
+	public boolean getCredentials() {
+		// TODO Auto-generated method stub
+		return false;
+	     db.executeQuery("INSERT INTO bookings VALUES(default,'"+this.getDateIn()+"','"+this.getDateOut()+"',"+this.getGID()+"," + this.getRID()+");");	  
+	     db.executeQuery("INSERT INTO bookings VALUES(default,'20/06/19','30/06/10',2,1);");
+	}
+}
